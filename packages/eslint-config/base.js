777447512281bcdc3019f474513e264d4af7e167
dworak <esteban@dworak.dev>
2025-06-30 13:07:48 -0600

    feat: added prettier and import auto sort to eslint

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -3,6 +3,8 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
 import onlyWarn from "eslint-plugin-only-warn";
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -16,9 +18,12 @@ export const config = [
   {
     plugins: {
       turbo: turboPlugin,
+        "simple-import-sort": simpleImportSort,
     },
     rules: {
       "turbo/no-undeclared-env-vars": "warn",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
   },
   {
@@ -29,4 +34,5 @@ export const config = [
   {
     ignores: ["dist/**"],
   },
+    eslintPluginPrettierRecommended,
 ];
