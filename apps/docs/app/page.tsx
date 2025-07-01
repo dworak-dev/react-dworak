8e1e360fcaba7851a9671e987c38ac24ea9d5f0b
dworak <esteban@dworak.dev>
2025-07-01 12:29:08 -0600

    feat: added shadcn to docs repo

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,16 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        fontFamily: "var(--font-renogare-regular)",
-      }}
-    >
-      dreamverse
-      <h1 className="text-3xl underline">Hello world!</h1>
+    <div className="flex flex-wrap items-center justify-center h-screen gap-2 md:flex-row">
+      <Button>Button</Button>
     </div>
   );
 }
