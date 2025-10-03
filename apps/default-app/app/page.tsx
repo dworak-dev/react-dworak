c6b281fedbd81faafb23dd4f07d363d9ac8b4e23
dworak <esteban@dworak.dev>
2025-10-03 13:17:00 -0600

    chore: release master

--- apps/default-app/app/page.tsx
+++ apps/default-app/app/page.tsx
@@ -57,7 +57,7 @@ export default () => {
           {
             /* x-release-please-start-version */
           }
-          throw new Error("This is a test error V1.2.8");
+          throw new Error("This is a test error V1.2.9");
           {
             /* x-release-please-end */
           }
@@ -144,7 +144,7 @@ export default () => {
         </CardFooter>
       </Card>
       {/* x-release-please-start-version */}
-      <span>V1.2.8</span>
+      <span>V1.2.9</span>
       {/* x-release-please-end */}
     </div>
   );
